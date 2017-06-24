a64d702016333ac62d8b7512b7ac0682305a5e32
Lionel Rivas <lionel.r.rivas@gmail.com>
2017-06-24 17:25:29 -0500

    Adding Vaadin 8 compatibility.

--- Flip-addon/src/main/java/org/vaadin/addons/flip/Flip.java
+++ Flip-addon/src/main/java/org/vaadin/addons/flip/Flip.java
@@ -40,13 +40,7 @@ public class Flip extends CustomComponent {
     private final Button flipFront = new Button(FontAwesome.COG);
     private final Button flipBack = new Button(FontAwesome.COG);
 
-    private final LayoutEvents.LayoutClickListener clickListener = new LayoutEvents.LayoutClickListener() {
-
-        @Override
-        public void layoutClick(LayoutEvents.LayoutClickEvent event) {
-            flip();
-        }
-    };
+    private final LayoutEvents.LayoutClickListener clickListener = event -> flip();
 
     private List<FlipListener> flipListeners = new ArrayList<>();
 
@@ -59,20 +53,10 @@ public class Flip extends CustomComponent {
         flipper.setSizeFull();
         root.addComponent(flipper);
 
-        flipFront.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                flip();
-            }
-        });
+        flipFront.addClickListener(event -> flip());
         flipper.addComponent(flipFront);
 
-        flipBack.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                flip();
-            }
-        });
+        flipBack.addClickListener(event -> flip());
         flipper.addComponent(flipBack);
 
         setFlipMode(FlipMode.MANUAL);
@@ -107,9 +91,7 @@ public class Flip extends CustomComponent {
         }
 
         // Notify listeners
-        for (FlipListener l : new ArrayList<>(flipListeners)) {
-            l.flipped(this);
-        };
+        flipListeners.forEach(flipListener -> flipListener.flipped(this));
 
     }
 
@@ -153,7 +135,7 @@ public class Flip extends CustomComponent {
         return front;
     }
 
-    public void setFrontComponent(Component frontSide) {
+    public final void setFrontComponent(Component frontSide) {
         if (this.front != null) {
             flipper.removeComponent(this.front);
         }
@@ -166,7 +148,7 @@ public class Flip extends CustomComponent {
         return back;
     }
 
-    public void setBackComponent(Component backSide) {
+    public final void setBackComponent(Component backSide) {
         if (this.back != null) {
             flipper.removeComponent(this.back);
         }
@@ -187,7 +169,7 @@ public class Flip extends CustomComponent {
         return flipMode;
     }
 
-    public void setFlipMode(FlipMode flipMode) {
+    public final void setFlipMode(FlipMode flipMode) {
         this.flipMode = flipMode;
         root.removeStyleName("hoverflip");
         flipFront.setVisible(flipMode == FlipMode.BUTTON);
--- Flip-addon/src/test/java/org/vaadin/addons/flip/FlipTest.java
+++ Flip-addon/src/test/java/org/vaadin/addons/flip/FlipTest.java
@@ -1,13 +1,63 @@
 package org.vaadin.addons.flip;
 
-import junit.framework.Assert;
+import com.vaadin.ui.Image;
 import org.junit.Test;
 
-// JUnit tests here
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.vaadin.addons.flip.Flip.FlipMode;
+
 public class FlipTest {
 
 	@Test
-	public void thisAlwaysPasses() {
-		Assert.assertEquals(true, true);
+	public void testSetFlipMode_should_be_manual_with_no_arg_constructor() {
+		Flip flip = new Flip();
+		assertEquals(FlipMode.MANUAL, flip.getFlipMode());
+	}
+
+	@Test
+	public void testSetFlipMode_on_click() {
+		Flip flip = new Flip();
+		flip.setFlipMode(FlipMode.ON_CLICK);
+		assertEquals(FlipMode.ON_CLICK, flip.getFlipMode());
+	}
+
+	@Test
+	public void testSetFrontAndBack() {
+		Flip flip = new Flip(new Image("Front Image"), new Image("Back Image"), FlipMode.MANUAL);
+
+		assertEquals("Front Image", flip.getFrontComponent().getCaption());
+		assertEquals("Back Image", flip.getBackComponent().getCaption());
+		assertEquals("front", flip.getFrontComponent().getStyleName());
+		assertEquals("back", flip.getBackComponent().getStyleName());
+	}
+
+	@Test
+	public void testFlip() {
+		Flip flip = new Flip(new Image("Front Image"), new Image("Back Image"), FlipMode.MANUAL);
+		assertFalse(flip.isBackVisible());
+		flip.flip();
+		assertTrue(flip.isBackVisible());
+		assertEquals("Front Image", flip.getFrontComponent().getCaption());
+		flip.flip();
+		assertEquals("Back Image", flip.getBackComponent().getCaption());
+
+		flip.flip();
+		assertEquals("Front Image", flip.getFrontComponent().getCaption());
+		flip.setFlipMode(FlipMode.ON_HOVER);
+
+		flip.flip();
+		assertEquals("Front Image", flip.getFrontComponent().getCaption());
+	}
+
+	@Test
+	public void testFlip_on_hover_should_not_flip() {
+		Flip flip = new Flip(new Image("Front Image"), new Image("Back Image"), FlipMode.ON_HOVER);
+		assertFalse(flip.isBackVisible());
+
+		flip.flip();
+		assertFalse(flip.isBackVisible());
+		assertEquals("Front Image", flip.getFrontComponent().getCaption());
 	}
 }
